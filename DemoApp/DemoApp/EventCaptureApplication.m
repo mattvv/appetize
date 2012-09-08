5ed7d56060de5041d90ff07209687c89fda3e3fd
Glen Tregoning <glen@gtbox.info>
2012-09-08 16:46:17 -0700

    Adding GLEssentials Project and Hacked in Event Capture Logic

---
+++ DemoApp/DemoApp/EventCaptureApplication.m
@@ -0,0 +1,13 @@
+//
+//  EventCaptureApplication.m
+//  DemoApp
+//
+//  Created by Glen Tregoning on 8/09/12.
+//  Copyright (c) 2012 Glen Tregoning. All rights reserved.
+//
+
+#import "EventCaptureApplication.h"
+
+@implementation EventCaptureApplication
+
+@end
