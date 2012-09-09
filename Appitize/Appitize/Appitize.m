5097d8d6e3c514f244870ec43d2fd68562bfaadd
glentregoning <glen@gtbox.info>
2012-09-08 17:37:14 -0700

    Removing Old

---
+++ Appitize/Appitize/Appitize.m
@@ -0,0 +1,13 @@
+//
+//  Appitize.m
+//  Appitize
+//
+//  Created by Glen Tregoning on 8/09/12.
+//  Copyright (c) 2012 Glen Tregoning. All rights reserved.
+//
+
+#import "Appitize.h"
+
+@implementation Appitize
+
+@end
---
+++ Appitize/AppitizeTests/AppitizeTests.h
@@ -0,0 +1,13 @@
+//
+//  AppitizeTests.h
+//  AppitizeTests
+//
+//  Created by Glen Tregoning on 8/09/12.
+//  Copyright (c) 2012 Glen Tregoning. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AppitizeTests : SenTestCase
+
+@end
