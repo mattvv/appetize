f55516ce11cfe3fec073c9964ab79da84ca22e0f
glentregoning <glen@gtbox.info>
2012-09-09 00:58:16 -0700

    Adding RecordControlViewController and hacked in integration

---
+++ Appitize/Appitize/RecordControlViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordControlViewController.h
+//  Appitize
+//
+//  Created by Glen Tregoning on 9/09/12.
+//  Copyright (c) 2012 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordControlViewController : UIViewController
+
+@end
---
+++ Appitize/Appitize/RecordControlViewController.m
@@ -0,0 +1,38 @@
+//
+//  RecordControlViewController.m
+//  Appitize
+//
+//  Created by Glen Tregoning on 9/09/12.
+//  Copyright (c) 2012 Google. All rights reserved.
+//
+
+#import "RecordControlViewController.h"
+
+@interface RecordControlViewController ()
+
+@end
+
+@implementation RecordControlViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
